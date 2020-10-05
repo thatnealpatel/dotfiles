88a74ca49ed1a6c7b4d285dc7345044f539a464f
Neal Patel (nealdotpy) <nealpatel2021@gmail.com>
2020-10-05 12:11:28 -0400

    changed from 5% to 10% pixelation

--- bin/scripts/blur_lock.sh
+++ bin/scripts/blur_lock.sh
@@ -5,7 +5,7 @@ img=$(mktemp $HOME/tmp/XXXXXXXXXX.png)
 import -window root $img 
 
 # Pixelate the screenshot
-convert $img -scale 5% -scale 2000% $img
+convert $img -scale 10% -scale 1000% $img
 
 i3lock -u -i $img
 
