956a0697d40b027dbf4a438310cff34fff686948
Neal Patel (nealdotpy) <nealpatel2021@gmail.com>
2020-10-06 11:39:07 -0400

    added exception case handling.

--- bin/scripts/get_pwd_branch.py
+++ bin/scripts/get_pwd_branch.py
@@ -11,7 +11,12 @@ from subprocess import check_output
 # dotgit -> '/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 res = '~'
 git = check_output(['ls', '-a']).decode('utf8')
-if git.find('.git ') > 0 or (git.find('.git') > 0 and git.find('.gitconfig') < 0): # we found a .git repo
+
+# a few exceptions to substrings of 'git'
+except_one = git.find('.gitconfig') < 0
+except_two = git.find('nealdotpy.github.io') < 0
+
+if git.find('.git ') > 0 or (git.find('.git') > 0 and except_one and except_two): # we found a .git repo
     res = check_output(['git', 'branch', '--show-current']).decode('utf8') #.split("* ")[1]
 elif git.find('.dotfiles') > 0:
 	res = '.dotfiles' # show the flag for .git -> .dotfiles (--bare) repo.
