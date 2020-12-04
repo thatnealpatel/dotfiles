4769ce3c9c8855261e6cc1c5e0cd2fa8aee3f305
nealdotpy <nealpatel2021@gmail.com>
2020-12-04 16:01:26 -0500

    testing new modules.

--- bin/scripts/stockbar/main.py
+++ bin/scripts/stockbar/main.py
@@ -1,9 +1,13 @@
 #!/home/neal/bin/scripts/stockbar/stonks/bin/python3
 
 import sys
-from cli_stonks.constants import Constants as constants
+from cli_stonks.constants import Constants as const
+from cli_stonks import quotes
 
-print(f'debug:\n{constants.WATCHLIST}')
+print(f'debug:\n{const.WATCHLIST}')
+
+symbols = ['$AAPL', '/es', 'bynd', '^vix']
+quotes.get_quotes(symbols)
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
@@ -11,9 +15,12 @@ if __name__ == "__main__":
         if operation == 'watchlist':
             # display_out = update_polybar_tape()
             # print(f'{display_out}') # polybar's final output
-        elif operation == 'acc_status':
+            pass
+        elif operation == 'status':
             # print(get_td_acc_status())
-        elif operation == 'get_quotes':
+            pass
+        elif operation == 'quotes':
             # print(display_terminal_quote(sys.argv[2:]))
+            pass
     else:
         print('Please provide a command line argument.')
