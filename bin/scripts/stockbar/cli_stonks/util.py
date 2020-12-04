d84a8409c84dcf9c2c5fc3e60580089c139c2358
nealdotpy <nealpatel2021@gmail.com>
2020-12-04 16:00:48 -0500

    added annualize, clean_symbols, and create_polybar_tape.

--- bin/scripts/stockbar/cli_stonks/util.py
+++ bin/scripts/stockbar/cli_stonks/util.py
@@ -1,18 +1,21 @@
 #!/home/neal/bin/scripts/stockbar/stonks/bin/python3
 
-import datetime, requests, json
+import datetime, requests, json 
+import typing as t
 from cli_stonks.constants import Constants as const
+
+
+def annualize(start_bal: float, curr_bal: float, time_in_days: int) -> float:
+    return (1 + (curr_bal - start_bal)/start_bal)**(365/time_in_days) - 1
+
 
 def refresh_access_token() -> None:
 
     current_str_time = str(datetime.datetime.now())
 
     try:
-        
-        # this is for polybar
-        print(f'{const.GREEN}Refreshing ACCESS_TOKEN...{const.CLEAR}')
+        print(f'{const.GREEN}Refreshing ACCESS_TOKEN...{const.CLEAR}') # for polybar
 
-        token_url = 'https://api.tdameritrade.com/v1/oauth2/token'
         headers = { 'Content-Type': 'application/x-www-form-urlencoded' }
         payload = {
             'grant_type': 'refresh_token', 
@@ -21,7 +24,7 @@ def refresh_access_token() -> None:
         }
 
         token_reply = requests.post(
-            'https://api.tdameritrade.com/v1/oauth2/token', 
+            const.TD_ENDPOINT_TOKEN, 
             headers=headers, data=payload
         ).json()
 
@@ -31,7 +34,46 @@ def refresh_access_token() -> None:
         with open(const.LOGFILE, 'a') as log: log.write(log_output)
 
     except Exception as e:
-
         print(f'{const.RED}An error occured updating ACCESS_TOKEN. See log.{const.CLEAR}')
         log_output = f'{current_str_time}: an error occured updating ACCESS_TOKEN:\n{e}\n'
         with open(logfile, 'a') as log: log.write(log_output)
+
+
+def clean_symbols(symbols: t.List[str]) -> t.List[str]:
+
+    cleaned = []
+
+    for symbol in symbols:
+        if symbol[0] == '^': # index
+            cleaned.append(f'${symbol[1:].upper()}.X')
+        elif symbol[0] == '$': # verbose stock symbol
+            cleaned.append(symbol[1:].upper())
+        else: # futures/stock symbol
+            cleaned.append(symbol.upper())
+
+    return cleaned
+
+
+def create_polybar_tape(symbol_data: t.List[t.Tuple]) -> None:
+    # symbol_data[i]: tuple<str, list> :: ('AAPL', [115.0, -2.04, false])
+    polybar_out = f''
+    
+    for symbol, data in symbol_data:
+        last_price = data[0]
+        per_change = data[1]
+        neg, flat, is_delayed = per_change < 0.0, per_change == 0.0, data[2]
+
+        # if negative -> .red else if flat -> .grey else -> .green
+        ticker_dir = [[const.GREEN, const.GREY][flat], const.RED][neg]
+
+        # make any delayed ticker show up as YELLOW instead of GREEN/RED
+        delayed = ['', const.YELLOW][is_delayed]
+
+        display_string = f'{ticker_dir}{delayed}{ticker}: '
+        display_string += f'{last_price} ({per_change}%){const.CLEAR}{const.MARGIN}'
+        
+        polybar_out += display_string
+
+    print(polybar_out)
+
+
