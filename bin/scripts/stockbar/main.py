b3525d27f21bf04fdbac944d1f7b9be1bbd7da1a
nealdotpy <nealpatel2021@gmail.com>
2020-12-07 17:23:15 -0500

    started quotes integration.

--- bin/scripts/stockbar/main.py
+++ bin/scripts/stockbar/main.py
@@ -5,12 +5,6 @@ from cli_stonks.constants import Constants as const
 from cli_stonks.quotes import get_quotes
 from cli_stonks.account import get_account_information
 
-print(f'debug:\n{const.WATCHLIST}')
-
-symbols = ['$AAPL', '/es', 'bynd', '^vix']
-print(get_quotes(symbols))
-
-
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
@@ -21,9 +15,7 @@ if __name__ == "__main__":
             pass
         elif operation == 'status':
             print(get_account_information())
-            pass
         elif operation == 'quotes':
-            # print(display_terminal_quote(sys.argv[2:]))
-            pass
+            print(get_quotes(sys.argv[2:]))
     else:
         print('Please provide a command line argument.')
