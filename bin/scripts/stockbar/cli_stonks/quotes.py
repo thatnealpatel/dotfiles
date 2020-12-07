38bc0304104bb0c9478d3dd2045208d02a03c9a4
nealdotpy <nealpatel2021@gmail.com>
2020-12-07 17:32:19 -0500

    removed cleaning responsibility

--- bin/scripts/stockbar/cli_stonks/quotes.py
+++ bin/scripts/stockbar/cli_stonks/quotes.py
@@ -1,11 +1,11 @@
 #!/home/neal/bin/scripts/stockbar/stonks/bin/python3
 
 import typing as t
-from cli_stonks.util import refresh_access_token, clean_symbols
+from cli_stonks.util import refresh_access_token
 
 def get_quotes(symbols: t.List[str]) -> t.List[t.Tuple]:
 
-    cleaned_symbols = clean_symbols(symbols)
-    return cleaned_symbols
+    return symbols
+    
     # error only:
     # refresh_access_token()
