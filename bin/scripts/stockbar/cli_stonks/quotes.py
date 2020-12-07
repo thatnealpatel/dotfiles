16ab2fb156defe0a07e1caf9693ccc9f0003a19e
nealdotpy <nealpatel2021@gmail.com>
2020-12-07 18:23:55 -0500

    finished polybar portion of quotes processing

--- bin/scripts/stockbar/cli_stonks/quotes.py
+++ bin/scripts/stockbar/cli_stonks/quotes.py
@@ -1,23 +1,54 @@
 #!/home/neal/bin/scripts/stockbar/stonks/bin/python3
 
 import typing as t
-from cli_stonks.util import refresh_access_token
+from cli_stonks.util import refresh_access_token, query_quotes, write_to_log, create_polybar_tape
 
-def get_quotes(symbols: t.List[str], fmt: str) -> t.List[t.Tuple]:
+def get_quotes(symbols: t.List[str], fmt: str) -> str:
         
     try:
-        if fmt == 'polybar': return get_quotes_polybar_fmt(symbols)
-        elif fmt == 'terminal': return get_quotes_term_fmt(symbols)
+        response = query_quotes(symbols)
+
+        if -1 in response:
+            return 'Refresh complete (hopefully).'
+        
+        symbols_data = extract_data(response, symbols)
+
+        if fmt == 'polybar': return create_polybar_tape(symbols_data)
+        elif fmt == 'terminal': return create_polybar_tape(symbols_data) #get_quotes_term_fmt(symbols_data)
     except Exception as e:
         print('An exception has occured in quotes.py')
-        # refresh_access_token()
+        print(e)
+        write_to_log(e)
 
     return 'unexpected behavior.'
 
 
-def get_quotes_term_fmt(symbols: t.List[str]) -> str:
+def extract_data(symbols_json: t.Dict, keys: t.List[str]) -> t.List[t.Tuple]:
+    symbols_data = []
+    
+    for symbol in keys:
+        last_price, percent_change = 'err', 'err'
+        delayed = symbols_json[symbol]['delayed']
+        if symbol[0] == '$': # was an index search e.g. $VIX.X $SPX.X
+            last_price = round(symbols_json[symbol]['lastPrice'], 2)
+            percent_change = round(symbols_json[symbol]['netPercentChangeInDouble'], 2)
+            symbol = f'^{symbol[1:-2]}'
+        elif symbol[0] == '/': # was a futures search e.g. /ES
+            last_price = round(symbols_json[symbol]['mark'], 2)
+            percent_change = round(symbols_json[symbol]['futurePercentChange']*100, 2)
+            symbol = f'{symbol}'
+        else: # was a regular quote e.g. aapl, goog
+            last_price = round(symbols_json[symbol]['mark'], 2)
+            percent_change = round(symbols_json[symbol]['markPercentChangeInDouble'], 2)
+            symbol = f'${symbol}'
+        symbols_data.append((symbol.lower(), [last_price, percent_change, delayed]))
+
+    return symbols_data
+
+
+def get_quotes_term_fmt(symbols: t.List[t.Tuple]) -> str:
     pass
 
 
-def get_quotes_polybar_fmt(symbols: t.List[str]) -> str:
+def get_quotes_polybar_fmt(symbols: t.List[t.Tuple]) -> str:
     pass 
