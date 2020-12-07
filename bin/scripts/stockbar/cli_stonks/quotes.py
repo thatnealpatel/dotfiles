409a501dd579b3cdf1c28251fcb5beacd4e3cda5
nealdotpy <nealpatel2021@gmail.com>
2020-12-07 17:38:18 -0500

    refactored terminal vs. polybar

--- bin/scripts/stockbar/cli_stonks/quotes.py
+++ bin/scripts/stockbar/cli_stonks/quotes.py
@@ -3,9 +3,21 @@
 import typing as t
 from cli_stonks.util import refresh_access_token
 
-def get_quotes(symbols: t.List[str]) -> t.List[t.Tuple]:
+def get_quotes(symbols: t.List[str], fmt: str) -> t.List[t.Tuple]:
+        
+    try:
+        if fmt == 'polybar': return get_quotes_polybar_fmt(symbols)
+        elif fmt == 'terminal': return get_quotes_term_fmt(symbols)
+    except Exception as e:
+        print('An exception has occured in quotes.py')
+        # refresh_access_token()
 
-    return symbols
-    
-    # error only:
-    # refresh_access_token()
+    return 'unexpected behavior.'
+
+
+def get_quotes_term_fmt(symbols: t.List[str]) -> str:
+    pass
+
+
+def get_quotes_polybar_fmt(symbols: t.List[str]) -> str:
+    pass 
