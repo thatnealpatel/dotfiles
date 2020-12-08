ba24cdb44f18ea74be453e384a7fd2371fb1dd9e
nealdotpy <nealpatel2021@gmail.com>
2020-12-07 19:02:47 -0500

    CLEAN CLEAN CLEAN CLEAN CODE

--- bin/scripts/stockbar/main.py
+++ bin/scripts/stockbar/main.py
@@ -4,20 +4,15 @@ import sys
 from cli_stonks.constants import Constants as const
 from cli_stonks.quotes import get_quotes
 from cli_stonks.account import get_account_information
-from cli_stonks.util import clean_symbols
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         operation = sys.argv[1]
         if operation == 'watchlist':
-            cleaned_symbols = clean_symbols(map(lambda x: ''.join(x).upper(), \
-                zip(const.WATCHLIST.values(), const.WATCHLIST.keys())))
-            print(get_quotes(cleaned_symbols, 'polybar'))
-            pass
+            print(get_quotes())
         elif operation == 'status':
             print(get_account_information())
         elif operation == 'quotes':
-            cleaned_symbols = clean_symbols(sys.argv[2:])
-            print(get_quotes(cleaned_symbols, 'terminal'))
+            print(get_quotes(sys.argv[2:], 'terminal'))
     else:
         print('Please provide a command line argument.')
