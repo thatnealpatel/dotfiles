56e98506fcec77a6418136a48eb0a3e5f4f051fd
nealdotpy <nealpatel2021@gmail.com>
2020-12-08 14:25:20 -0500

    display risk-free rate.

--- bin/scripts/stockbar/cli_stonks/account.py
+++ bin/scripts/stockbar/cli_stonks/account.py
@@ -33,7 +33,7 @@ def create_account_summary(response: t.Dict) -> str:
     port_return = (net_liq - const.TDA_PRINCIPLE) / const.TDA_PRINCIPLE
     annualized_return = annualize(const.TDA_PRINCIPLE, net_liq, period_in_days.days)
     
-    current_sharpe_ratio, annualized_sharpe_ratio = calculate_sharpe(port_return, annualized_return)
+    current_sharpe_ratio, annualized_sharpe_ratio, rfr = calculate_sharpe(port_return, annualized_return)
 
     # do the rounding for displaying
     port_return, annualized_return = round(port_return*100, 2), round(annualized_return*100, 2)
@@ -50,12 +50,13 @@ def create_account_summary(response: t.Dict) -> str:
     fmt_port_return = f'return({period_in_days.days}){port_return:>14}%'
     annualized_return = f'annualized{annualized_return:>15}%' 
     fmt_annualized = f'{const.TERM_YELLOW_TEXT}{annualized_return}{const.TERM_RESET}'
-    fmt_curr_sharpe = f'sharpe:{current_sharpe_ratio:>19}'
-    annualized_sharpe = f'annualized sharpe:{annualized_sharpe_ratio:>8}'
+    fmt_current_rfr = f'risk-free rate{rfr*100:>11}%'
+    fmt_curr_sharpe = f'sharpe{current_sharpe_ratio:>20}'
+    annualized_sharpe = f'annualized sharpe{annualized_sharpe_ratio:>9}'
     fmt_ann_sharpe = f'{const.TERM_YELLOW_TEXT}{annualized_sharpe}{const.TERM_RESET}'
 
     return f'{fmt_avail_funds}\n{fmt_net_liq}\n{fmt_equity}\n{fmt_port_return}\n{fmt_annualized}\
-                \n{fmt_curr_sharpe}\n{fmt_ann_sharpe}'
+                \n{fmt_current_rfr}\n{fmt_curr_sharpe}\n{fmt_ann_sharpe}'
 
 
 def create_position_summary(response: t.Dict) -> str:
