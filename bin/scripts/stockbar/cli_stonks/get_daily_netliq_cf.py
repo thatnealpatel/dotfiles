5fc3d68e237b8aa9cc2eb459a5329285f465aafb
nealdotpy <nealpatel2021@gmail.com>
2020-12-10 15:27:36 -0500

    wrote cloud function script. tested locally. working.

--- bin/scripts/stockbar/cli_stonks/get_daily_netliq_cf.py
+++ bin/scripts/stockbar/cli_stonks/get_daily_netliq_cf.py
@@ -3,6 +3,68 @@ Google Cloud Platform
 Cloud Function
 Executed via Cloud Task
 
-Purpose: Get daily net liquidiation value in order to accurately calculate standard deviations
+Purpose: Get daily net liquidition value in order to accurately calculate standard deviations
 in portfolio returns which in turn is used to calculate sharpe ratios and adjust strategies.
 '''
+
+# LOCAL ONLY
+from dotenv import set_key, load_dotenv, find_dotenv
+dotenv_file = find_dotenv()
+load_dotenv(dotenv_file)
+# LOCAL ONLY
+
+import os, requests, datetime, pandas
+from google.cloud import bigquery
+
+TDA_ID = os.environ.get('TDA_ID')
+API_KEY = os.environ.get('API_KEY')
+REFRESH_TOKEN = os.environ.get('REFRESH_TOKEN')
+
+DAYS_SINCE_START = (datetime.date.today() - datetime.date(2020, 8, 24)).days
+
+TD_ENDPOINT_TOKEN = f'https://api.tdameritrade.com/v1/oauth2/token'
+TD_ENDPOINT_ACCOUNTS = f'https://api.tdameritrade.com/v1/accounts/{TDA_ID}?'
+
+def get_access_token():
+    try:     
+        headers = { 'Content-Type': 'application/x-www-form-urlencoded' }
+        payload = {
+            'grant_type': 'refresh_token', 
+            'client_id': API_KEY, 
+            'refresh_token': REFRESH_TOKEN
+        }
+        token_reply = requests.post(
+            TD_ENDPOINT_TOKEN, 
+            headers=headers, data=payload
+        ).json()
+
+        return token_reply['access_token']
+    except Exception as e:
+        print(f'An error occured while fetching the access token.\n  {e}')
+
+
+def get_netliq():
+    try:
+        payload = {'fields': 'positions,orders'}
+        headers = {
+            'Accept': 'application/json',
+            'Authorization': f'Bearer {get_access_token()}'
+        }
+
+        acc_reply = requests.get(url=TD_ENDPOINT_ACCOUNTS, params=payload, headers=headers).json()
+        net_liq = acc_reply['securitiesAccount']['currentBalances']['liquidationValue']
+        
+        return float(net_liq)
+    except Exception as e:
+        print(f'An error occured while fetching the net liquidition value.\n  {e}')
+
+
+def write_to_bigquery_table(event = None, context = None):
+    client = bigquery.Client()
+    table_id = 'cli-stocks.daily_portfolio_stats.daily_netliq'
+    payload = [{u'day': DAYS_SINCE_START, u'netliq': get_netliq()}]
+    errors = client.insert_rows_json(table_id, payload)
+    if not errors:
+        print('Successfully added netliq payload to the BQ Table.')
+    else:
+        print(f'Encountered error(s) while inserting payload:\n{errors}')
