7f03d5221497db093e561ef6a2cf8da050303f64
nealdotpy <nealpatel2021@gmail.com>
2020-12-10 12:45:54 -0500

    skeleton for google cloud function to store daily netliq in bq table.

---
+++ bin/scripts/stockbar/cli_stonks/get_daily_netliq_cf.py
@@ -0,0 +1,8 @@
+'''
+Google Cloud Platform
+Cloud Function
+Executed via Cloud Task
+
+Purpose: Get daily net liquidiation value in order to accurately calculate standard deviations
+in portfolio returns which in turn is used to calculate sharpe ratios and adjust strategies.
+'''
